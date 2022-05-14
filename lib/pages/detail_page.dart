a60aab6fb1c5f5e80c2db1508882eea0f1d1af70
mansur <mansuriosdevm1@gmail.com>
2022-05-14 13:40:38 +0500

    new battn

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
-  static final String id="detail_page";
-
-  const DetailPage({Key? key}) : super(key: key);
+  static final String id = "detail_page";
+  //const DetailPage({Key? key}) : super(key: key);
+  final String input1;
+  final int input2;
+  DetailPage({required this.input1, required this.input2});
 
   @override
   State<DetailPage> createState() => _DetailPageState();
@@ -12,6 +14,16 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          onPressed: (){
+            Navigator.of(context).pop({'data':"Dart 22"});
+          },
+          color: Colors.red,
+          child: Text(widget.input1),
+        )
+      ),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pdp/pages/detail_page.dart';
 
 class HomePage extends StatefulWidget {
   static final String id="Home_page";
-
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -10,8 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String data = "Open detail page";
+  Future _openDetails() async {
+    Map result = await Navigator.of(context).push(MaterialPageRoute(
+        builder: (BuildContext context) {
+          return new DetailPage(input1: "Flutter", input2: 22,);
+        }
+    ));
+
+    if (result != null && result.containsKey ('data')){
+      setState(() {
+        data = result['data'];
+      });
+
+    }else{
+    print("Nothing");
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          onPressed: (){
+            _openDetails();
+            //Navigator.pushNamed(context, DetailPage.id);
+          },
+          color: Colors.blue,
+          child: Text(data),
+        ),
+      ),
+    );
   }
 }
